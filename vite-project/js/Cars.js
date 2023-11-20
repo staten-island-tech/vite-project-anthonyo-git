8645571a04767338a483f062074dbc93a77f1e21
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2023-11-19 22:04:37 -0500

    s

---
+++ vite-project/js/Cars.js
@@ -0,0 +1,88 @@
+const Cars = [
+    {
+        "brand": "Hyundai",
+        "description": "Hyundai Sonata is a reliable sedan with advanced safety features.",
+        "category": "Sedan",
+        "type": "Electric",
+        "origin": "South Korea"
+    },
+    {
+        "brand": "Volkswagen",
+        "description": "Volkswagen Golf is a versatile hatchback known for its performance.",
+        "category": "Hatchback",
+        "type": "Gasoline",
+        "origin": "Germany"
+    },
+    {
+        "brand": "BMW",
+        "description": "BMW 5 Series is a luxury sedan that combines style with cutting-edge technology.",
+        "category": "Luxury",
+        "type": "Hybrid",
+        "origin": "Germany"
+    },
+    {
+        "brand": "Mercedes",
+        "description": "Mercedes-Benz C-Class offers a perfect blend of elegance and performance.",
+        "category": "Luxury",
+        "type": "Electric",
+        "origin": "Germany"
+    },
+    {
+        "brand": "Tesla",
+        "description": "Tesla Model 3 is an all-electric sedan with impressive range and acceleration.",
+        "category": "Electric",
+        "type": "Electric",
+        "origin": "United States"
+    },
+    {
+        "brand": "Porsche",
+        "description": "Porsche 911 is a legendary sports car known for its iconic design and powerful performance.",
+        "category": "Sports Car",
+        "type": "Gasoline",
+        "origin": "Germany"
+    },
+    {
+        "brand": "Audi",
+        "description": "Audi A4 combines performance and luxury, making it a popular choice in its class.",
+        "category": "Luxury",
+        "type": "Hybrid",
+        "origin": "Germany"
+    },
+    {
+        "brand": "Fiat",
+        "description": "Fiat 500 is a compact car with a stylish design and efficient fuel economy.",
+        "category": "Compact",
+        "type": "Gasoline",
+        "origin": "Italy"
+    },
+    {
+        "brand": "Mini",
+        "description": "Mini Cooper is a small car that delivers a fun and agile driving experience.",
+        "category": "Compact",
+        "type": "Gasoline",
+        "origin": "United Kingdom"
+    },
+    {
+        "brand": "Smart",
+        "description": "Smart EQ ForTwo is a compact electric car designed for urban mobility.",
+        "category": "Compact",
+        "type": "Electric",
+        "origin": "Germany"
+    },
+    {
+        "brand": "Alfa Romeo",
+        "description": "Alfa Romeo Giulia is a stylish and performance-oriented luxury sedan.",
+        "category": "Luxury",
+        "type": "Gasoline",
+        "origin": "Italy"
+    },
+    {
+        "brand": "Land Rover",
+        "description": "Land Rover Range Rover is a premium SUV known for its off-road capability and luxury features.",
+        "category": "SUV",
+        "type": "Hybrid",
+        "origin": "United Kingdom"
+    }
+];
+
+export { Cars };
